4a5b1970acb03d24bb8c23ed797ea124363c3833
Muhammed Thanish <mnmtanish@gmail.com>
2019-02-14 06:12:17 +0530

    Change how stories are stored

--- src/application/models/story.js
+++ src/application/models/story.js
@@ -1,14 +1,32 @@
 import * as orderBy from 'lodash/orderBy';
 import { currentDate, equalDates } from '../../framework/datetime';
 
+function migrateStories() {
+  const version = localStorage.getItem('pereda@version') || 0;
+  let newVersion = 0;
+  if (version < 1) {
+    for (const maybeId in localStorage) {
+      if (maybeId.startsWith('pereda:')) {
+        const id = maybeId.slice(7);
+        const date = id.split('.').map(s => parseInt(s, 10));
+        const text = localStorage.getItem(maybeId);
+        localStorage.removeItem(maybeId);
+        localStorage.setItem(`pereda:story:${id}`, JSON.stringify({ id, date, text }));
+      }
+    }
+    newVersion = 1;
+  }
+  if (version !== newVersion) {
+    localStorage.setItem('pereda@version', newVersion);
+  }
+}
+
 export function loadStories() {
+  migrateStories();
   const stories = [];
   for (const maybeId in localStorage) {
-    if (maybeId.startsWith('pereda:')) {
-      const id = maybeId.slice(7);
-      const date = id.split('.').map(s => parseInt(s, 10));
-      const text = localStorage.getItem(maybeId);
-      stories.push({ id, date, text });
+    if (maybeId.startsWith('pereda:story')) {
+      stories.push(JSON.parse(localStorage.getItem(maybeId)));
     }
   }
   const currDate = currentDate();
@@ -21,6 +39,6 @@ export function loadStories() {
 
 export function storeStories(stories) {
   for (const story of stories) {
-    localStorage.setItem(`pereda:${story.id}`, story.text);
+    localStorage.setItem(`pereda:story:${story.id}`, story.text);
   }
 }
